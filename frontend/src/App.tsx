d14fc1b0e9e61b77dc83c9cda7cd1bcb60365c45
Fellipe <fellipe01235@gmail.com>
2022-07-15 16:30:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
