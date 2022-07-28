7f1e1c4c9057135ecef88b4766375d52ee7821ce
Fellipe <fellipe01235@gmail.com>
2022-07-27 21:50:48 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
 import  './styles.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleCick (id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+        console.log("Sucesso")
+    })
+}
+
+function NotificationButton( {saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleCick(saleId)}>
             <img src={icon} alt="Notificar" />
         </div>
     )
